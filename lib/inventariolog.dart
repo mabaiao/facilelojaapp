e7964c45bfe45fbf7e8d3078682c8ebdcc132075
mabaiao <mabaiao@gmail.com>
2024-08-12 20:44:41 -0300

    update

--- lib/inventariolog.dart
+++ lib/inventariolog.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-import 'dart:io';
 
 import 'package:facilelojaapp/main.dart';
 import 'package:facilelojaapp/util.dart';
--- lib/menu.dart
+++ lib/menu.dart
@@ -10,7 +10,6 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart' hide ModalBottomSheetRoute;
 import 'package:flutter/services.dart';
 import 'package:flutter_animate/flutter_animate.dart';
-import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
 
 //import 'caixa.dart.txt';
 // import 'filaatendimento.dart';
